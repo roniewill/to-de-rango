ca723d67ca189b11dbc39eaeadc42e500476c6d6
Ramon William <roniewill@gmail.com>
2021-07-13 17:08:53 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  resources :products, only: [:index]
+  resources :products, only: [:index, :new, :create]
   resources :restaurants
   devise_for :users
   root 'home#index'
