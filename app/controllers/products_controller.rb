387eb2f8b8dc73563f96b35a3844588861ca9337
Ramon William <roniewill@gmail.com>
2021-07-13 17:08:18 -0300

    Add methods new and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,32 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { render :index, notice: "Product created" }
+      else
+        format.html { render :new, alert: "Error to create product" } 
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:name, :value, :restaurant_id)
+    end
 end
