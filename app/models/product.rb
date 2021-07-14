31b856641c1536512fc7d54aa740a6bc93e84591
Ramon William <roniewill@gmail.com>
2021-07-14 12:51:25 -0300

    Add validate fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :restaurant
+
+  validates :name, presence: true
+  validates :value, presence: true
+  validates :restaurant_id, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it { should belong_to(:restaurant) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :value }
+  it { should validate_presence_of :restaurant_id }
 end
